3a86d98299794ace2137211dfcf412f9fc39fa9d
Kiran Singh <kiran.singh@globtier.in>
2023-06-08 17:03:47 +0530

    created graphql queries and mutation with the react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from '@apollo/client'
+import Persons from './components/Persons'
+import { ALL_PERSONS } from './queries'
+import PersonForm from './components/PersonsForm'
+import { useState } from 'react'
+import PhoneForm from './components/PhoneForm'
 
-function App() {
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
+
+  const result = useQuery(ALL_PERSONS , {
+    pollInterval: 2000
+  })
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
-  );
+  )
 }
 
-export default App;
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null
+  }
+  return (
+    <div style={{color: 'red'}}>
+    {errorMessage}
+    </div>
+  )
+}
+
+export default App
